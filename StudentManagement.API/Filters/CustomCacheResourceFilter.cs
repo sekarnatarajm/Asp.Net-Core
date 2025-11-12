adde71e17b45c1339a4153b8f254bfedd1d1e693
Sekar_Nataraj <Sekar.Nataraj@dellteam.com>
2025-11-12 19:39:33 +0530

    Action Filter implemented

---
+++ StudentManagement.API/Filters/CustomCacheResourceFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace StudentManagement.API.Filters
+{
+    public class CustomCacheResourceFilter : IResourceFilter
+    {
+        private readonly IMemoryCache _cache;
+        public CustomCacheResourceFilter(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            var cacheKey = context.HttpContext.Request.Path.ToString();
+            if (_cache.TryGetValue(cacheKey, out IActionResult cachedResult))
+            {
+                context.Result = cachedResult; // Short-circuit the pipeline
+            }
+        }
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            var cacheKey = context.HttpContext.Request.Path.ToString();
+            if (!_cache.TryGetValue(cacheKey, out _))
+            {
+                _cache.Set(cacheKey, context.Result, TimeSpan.FromMinutes(5));
+            }
+        }
+    }
+}
